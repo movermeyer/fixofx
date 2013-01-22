ae4e880c60c4ee4db675f03a39dd55e738d78422
Henrique Bastos <henrique@bastos.net>
2013-01-22 15:11:12 -0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='fixofx',
+    version='',
+    description='',
+    long_description='',
+    author='',
+    author_email='',
+    url='',
+    #packages=[],
+    #install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'fixofx = fixofx',
+         ]
+    },
+    license='Apache 2.0',
+    keywords='',
+    classifiers=[])
